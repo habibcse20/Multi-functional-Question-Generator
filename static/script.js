7b0f213d4d495902750aaf2a4a75a53c4d2eb0d6
Md. Habibur Rahman <habib.cse20@gmail.com>
2026-01-22 07:52:32 -0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,19 @@
+function switchTab(tabName) {
+    document.querySelectorAll('.tab').forEach(t => t.classList.remove('active'));
+    document.querySelectorAll('.content').forEach(c => c.classList.remove('active'));
+
+    document.getElementById(tabName + '-tab').classList.add('active');
+    document.getElementById(tabName).classList.add('active');
+}
+
+function toggleAnswer(btn) {
+    const ans = btn.nextElementSibling;
+    if (ans.style.display === "none") {
+        ans.style.display = "block";
+        btn.innerText = "Hide Answer";
+    } else {
+        ans.style.display = "none";
+        btn.innerText = "Show Answer";
+    }
+}
 
